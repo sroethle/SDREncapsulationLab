c187a6bc87d91b2ecf699e9320cbd6dcc1632b9c
sroethle <sroethle@L11704038600.wctc.msft>
2012-08-29 18:15:18 -0500

    Changed my mind.

---
+++ src/sdrencapsulationlab/SecondClass.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sdrencapsulationlab;
+
+/**
+ *
+ * @author sroethle
+ */
+public class SecondClass {
+    private int x;
+    
+}
